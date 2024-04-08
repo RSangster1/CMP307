97494967a23c2928789914fc04dce05347e5a87f
Robbie Sangster <114151269+RSangster1@users.noreply.github.com>
2024-04-08 12:59:42 +0100

    Add files via upload

---
+++ Part1/307 Coursework/307 Coursework/mssql2003115DataSet.cs
@@ -0,0 +1,15 @@
+﻿namespace _307_Coursework
+{
+
+
+    partial class mssql2003115DataSet
+    {
+    }
+}
+
+namespace _307_Coursework.mssql2003115DataSetTableAdapters {
+    
+    
+    public partial class AssetsTableAdapter {
+    }
+}
